6fe13083ac8579fa5ca469ff997c82f999f3c62b
alidogangamuse <alidogan915@gmail.com>
2018-04-14 15:06:47 +0300

    Add project files.

---
+++ E-Ticaret/Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace E_Ticaret.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Login> Logins { get; set; }
+    }
+}
---
+++ E-Ticaret/Models/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace E_Ticaret.Models
+{
+    [Table("Kullanıcı")]
+    public class Login
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(100),Required]
+        public string KullaniciAdi { get; set; }
+
+        [StringLength(100),Required]
+        public string Sifre { get; set; }
+    }
+}
